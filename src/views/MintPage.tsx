6f74c8caff873f7436dd44e42ef046cc26acc2d5
jpngeekudin <udinshalah12@gmail.com>
2022-07-05 16:25:04 +0700

    add footer on mint page

--- src/views/MintPage.tsx
+++ src/views/MintPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar'
 import ProgressBar from '../components/ProgressBar'
 
@@ -74,7 +75,7 @@ export default function MintPage() {
         </div>
       </div>
 
-      <div className='row row-cols-2 g-5'>
+      <div className='row row-cols-2 g-5 mb-5'>
         <div>
           <div className="fw-bold" style={{ fontSize: 48 }}>About</div>
           <div className="mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.</div>
@@ -112,6 +113,8 @@ export default function MintPage() {
 
         </div>
       </div>
+
+      <Footer/>
     </div>
   )
 }
