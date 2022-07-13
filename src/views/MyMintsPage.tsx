34b2b86ef6650068451746a981fa2c90f7714eca
jpngeekudin <udinshalah12@gmail.com>
2022-07-13 13:44:45 +0700

    mymintspage

--- src/views/MyMintsPage.tsx
+++ src/views/MyMintsPage.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
 
 export default function MyMintsPage() {
   const [showForm, setShowForm] = useState(false);
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors }, control } = useForm();
 
   return (
     <>
@@ -60,19 +61,134 @@ export default function MyMintsPage() {
           <div className="row row-cols-2 mb-4">
             <div>
               <div className="label-with-sublabel">
-                <div className='label'>NFT Name</div>
-                <div className='sublabel'>What is the collection name</div>
+                <div className='label'>Public Date/Time (UTC)</div>
+                <div className='sublabel'>Public sale start time</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='Signal Boost' {...register('nftName')} />
+              <Controller
+                name='publicDateTime'
+                control={control}
+                rules={{ required: true }}
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error }
+                }) => (
+                  <DatePicker
+                    selected={value}
+                    onChange={value => {
+                      console.log('onChange', value);
+                      onChange(value);
+                    }}
+                    showTimeSelect
+                    className='form-control form-control-grey'
+                    dateFormat={'yyyy/MM/dd HH:mm:ss'}
+                    ref={ref}
+                    placeholderText='yyyy/MM/dd HH:mm:ss'
+                  />
+                )}
+              />
             </div>
             <div>
               <div className="label-with-sublabel">
-                <div className='label'>Project Symbol</div>
-                <div className='sublabel'>Similiar to stock ticker</div>
+                <div className='label'>Public Price</div>
+                <div className='sublabel'>Public sale price</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='SB' {...register('projectSymbol')} />
+              <input type="number" className="form-control form-control-grey" placeholder='1.5' {...register('publicPrice')} />
             </div>
           </div>
+          <div className="row mb-4">
+            <div className="col-lg-8">
+              <div className="label-with-sublabel">
+                <div className="label">Primary Sales Address</div>
+                <div className="sublabel">This is where proceeds from the mint will go</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+            </div>
+            <div className="col-lg-4"></div>
+          </div>
+          <div className="row mb-4">
+            <div className="col-lg-8">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Address 1</div>
+                <div className="sublabel">This is where proceeds from secondary sales will go</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+            </div>
+            <div className="col-lg-4">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Share 1</div>
+                <div className="sublabel">Percentage for secondary shareholder</div>
+              </div>
+              <input type="number" className="form-control form-control-grey" placeholder='30' />
+            </div>
+          </div>
+          <div className="row mb-4">
+            <div className="col-lg-8">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Address 2 (optional)</div>
+                <div className="sublabel">This is where proceeds from secondary sales will go</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+            </div>
+            <div className="col-lg-4">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Share 2 (optional)</div>
+                <div className="sublabel">Percentage for secondary shareholder</div>
+              </div>
+              <input type="number" className="form-control form-control-grey" placeholder='30' />
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col-lg-8">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Address 3 (optional)</div>
+                <div className="sublabel">This is where proceeds from secondary sales will go</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+            </div>
+            <div className="col-lg-4">
+              <div className="label-with-sublabel">
+                <div className="label">Secondary Share 3 (optional)</div>
+                <div className="sublabel">Percentage for secondary shareholder</div>
+              </div>
+              <input type="number" className="form-control form-control-grey" placeholder='30' />
+            </div>
+          </div>
+
+          {/* form social media */}
+          <div className="row row-cols-3 mb-4">
+            <div>
+              <div className="label-with-sublabel">
+                <div className="label">Website</div>
+                <div className="sublabel">Link to website</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='http://venatici.io' />
+            </div>
+            <div>
+              <div className="label-with-sublabel">
+                <div className="label">Discord</div>
+                <div className="sublabel">Link to discord</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='http://discord.gg/siviw2fasc' />
+            </div>
+            <div>
+              <div className="label-with-sublabel">
+                <div className="label">Twitter</div>
+                <div className="sublabel">Link to twitter</div>
+              </div>
+              <input type="text" className="form-control form-control-grey" placeholder='http://twitter.com/venatici' />
+            </div>
+          </div>
+
+          <div className="mb-5">
+            <div className="label-with-sublabel">
+              <div className="label">Anything we should know?</div>
+              <div className="sublabel">Any additional details go here</div>
+            </div>
+            <textarea className="form-control form-control-grey" rows={4} placeholder="Signal boost not only an Art but messages for the holder."></textarea>
+          </div>
+          <button className="btn btn-black-gradient px-4">
+            <div className="content">Submit Form</div>
+          </button>
         </div>
       )}
 
