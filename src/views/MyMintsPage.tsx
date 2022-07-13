c6f245b44bd80b556f87365872185136632c55cd
jpngeekudin <udinshalah12@gmail.com>
2022-07-13 23:39:17 +0700

    my mints form validation

--- src/views/MyMintsPage.tsx
+++ src/views/MyMintsPage.tsx
@@ -1,10 +1,40 @@
 import React, { useState } from 'react'
-import { Controller, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+type MyMintsFormData = {
+  nftName: string,
+  projectSymbol: string,
+  projectSupply: number,
+  royaltyPercentage: number,
+  nftDescription: string,
+  publicDateTime: Date,
+  publicPrice: number,
+  primaryAddress: string,
+  secondaryAddress1: string,
+  secondaryAddress2: string,
+  secondaryAddress3: string,
+  secondaryShare1: number,
+  secondaryShare2: number,
+  secondaryShare3: number,
+  website: string,
+  discord: string,
+  twitter: string,
+  anythingElse: string
+}
 
 export default function MyMintsPage() {
   const [showForm, setShowForm] = useState(false);
-  const { register, handleSubmit, watch, formState: { errors }, control } = useForm();
+  const { register, handleSubmit, watch, formState: { errors }, control } = useForm<MyMintsFormData>();
+  const navigate = useNavigate();
+
+  const submit: SubmitHandler<MyMintsFormData> = (data) => {
+    console.log(data);
+    toast.success('Thanks for submitting your mints!');
+    navigate('/mint/solana');
+  }
 
   return (
     <>
@@ -25,14 +55,16 @@ export default function MyMintsPage() {
                 <div className='label'>NFT Name</div>
                 <div className='sublabel'>What is the collection name</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='Signal Boost' {...register('nftName')} />
+              <input type="text" className="form-control form-control-grey" placeholder='Signal Boost' {...register('nftName', { required: true })} />
+              {errors.nftName && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
             <div>
               <div className="label-with-sublabel">
                 <div className='label'>Project Symbol</div>
                 <div className='sublabel'>Similiar to stock ticker</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='SB' {...register('projectSymbol')} />
+              <input type="text" className="form-control form-control-grey" placeholder='SB' {...register('projectSymbol', { required: true })} />
+              {errors.projectSymbol && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
           </div>
           <div className="row row-cols-2 mb-4">
@@ -41,14 +73,16 @@ export default function MyMintsPage() {
                 <div className='label'>Project Supply</div>
                 <div className='sublabel'>How many NFT will be created</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='1111' {...register('projectSupply')} />
+              <input type="number" className="form-control form-control-grey" placeholder='1111' {...register('projectSupply', { required: true })} />
+              {errors.projectSupply && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
             <div>
               <div className="label-with-sublabel">
                 <div className='label'>Royalty Percentage</div>
                 <div className='sublabel'>This is for secondary sales</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='5' {...register('royaltyPercentage')} />
+              <input type="number" className="form-control form-control-grey" placeholder='5' {...register('royaltyPercentage', { required: true })} />
+              {errors.royaltyPercentage && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
           </div>
           <div className="mb-4">
@@ -56,7 +90,8 @@ export default function MyMintsPage() {
               <div className='label'>NFT Description</div>
               <div className='sublabel'>This description for your NFT will appear in the owners wallets.</div>
             </div>
-            <textarea className='form-control form-control-grey' rows={4} placeholder='1111SIgnal Boost are spreading on Solana.' {...register('nftDescription')}></textarea>
+            <textarea className='form-control form-control-grey' rows={4} placeholder='1111SIgnal Boost are spreading on Solana.' {...register('nftDescription', { required: true })}></textarea>
+              {errors.nftDescription && <div className='text-danger mt-3'>This field is required!</div>}
           </div>
           <div className="row row-cols-2 mb-4">
             <div>
@@ -86,13 +121,15 @@ export default function MyMintsPage() {
                   />
                 )}
               />
+              {errors.publicDateTime && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
             <div>
               <div className="label-with-sublabel">
                 <div className='label'>Public Price</div>
                 <div className='sublabel'>Public sale price</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='1.5' {...register('publicPrice')} />
+              <input type="number" className="form-control form-control-grey" placeholder='1.5' {...register('publicPrice', { required: true })} />
+              {errors.publicPrice && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
           </div>
           <div className="row mb-4">
@@ -101,7 +138,8 @@ export default function MyMintsPage() {
                 <div className="label">Primary Sales Address</div>
                 <div className="sublabel">This is where proceeds from the mint will go</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' {...register('primaryAddress', { required: true })} />
+              {errors.primaryAddress && <div className='text-danger mt-3'>This field is required!</div>}
             </div>
             <div className="col-lg-4"></div>
           </div>
@@ -111,14 +149,14 @@ export default function MyMintsPage() {
                 <div className="label">Secondary Address 1</div>
                 <div className="sublabel">This is where proceeds from secondary sales will go</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' {...register('secondaryAddress1')} />
             </div>
             <div className="col-lg-4">
               <div className="label-with-sublabel">
                 <div className="label">Secondary Share 1</div>
                 <div className="sublabel">Percentage for secondary shareholder</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='30' />
+              <input type="number" className="form-control form-control-grey" placeholder='30' {...register('secondaryShare1')} />
             </div>
           </div>
           <div className="row mb-4">
@@ -127,14 +165,14 @@ export default function MyMintsPage() {
                 <div className="label">Secondary Address 2 (optional)</div>
                 <div className="sublabel">This is where proceeds from secondary sales will go</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' {...register('secondaryAddress2')} />
             </div>
             <div className="col-lg-4">
               <div className="label-with-sublabel">
                 <div className="label">Secondary Share 2 (optional)</div>
                 <div className="sublabel">Percentage for secondary shareholder</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='30' />
+              <input type="number" className="form-control form-control-grey" placeholder='30' {...register('secondaryShare2')} />
             </div>
           </div>
           <div className="row mb-5">
@@ -143,14 +181,14 @@ export default function MyMintsPage() {
                 <div className="label">Secondary Address 3 (optional)</div>
                 <div className="sublabel">This is where proceeds from secondary sales will go</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' />
+              <input type="text" className="form-control form-control-grey" placeholder='0x3B6Bfd8f885e33e7E75d810DC564e33c79EDFAd7' {...register('secondaryAddress3')} />
             </div>
             <div className="col-lg-4">
               <div className="label-with-sublabel">
                 <div className="label">Secondary Share 3 (optional)</div>
                 <div className="sublabel">Percentage for secondary shareholder</div>
               </div>
-              <input type="number" className="form-control form-control-grey" placeholder='30' />
+              <input type="number" className="form-control form-control-grey" placeholder='30' {...register('secondaryShare3')} />
             </div>
           </div>
 
@@ -161,21 +199,21 @@ export default function MyMintsPage() {
                 <div className="label">Website</div>
                 <div className="sublabel">Link to website</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='http://venatici.io' />
+              <input type="text" className="form-control form-control-grey" placeholder='http://venatici.io' {...register('website')} />
             </div>
             <div>
               <div className="label-with-sublabel">
                 <div className="label">Discord</div>
                 <div className="sublabel">Link to discord</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='http://discord.gg/siviw2fasc' />
+              <input type="text" className="form-control form-control-grey" placeholder='http://discord.gg/siviw2fasc' {...register('discord')} />
             </div>
             <div>
               <div className="label-with-sublabel">
                 <div className="label">Twitter</div>
                 <div className="sublabel">Link to twitter</div>
               </div>
-              <input type="text" className="form-control form-control-grey" placeholder='http://twitter.com/venatici' />
+              <input type="text" className="form-control form-control-grey" placeholder='http://twitter.com/venatici' {...register('twitter')} />
             </div>
           </div>
 
@@ -187,7 +225,7 @@ export default function MyMintsPage() {
             <textarea className="form-control form-control-grey" rows={4} placeholder="Signal boost not only an Art but messages for the holder."></textarea>
           </div>
           <button className="btn btn-black-gradient px-4">
-            <div className="content">Submit Form</div>
+            <div className="content" onClick={handleSubmit(submit)}>Submit Form</div>
           </button>
         </div>
       )}
